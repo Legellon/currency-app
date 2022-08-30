0aa24df85ba3568f3c49e850e9a5547fc4ded912
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-08-30 19:02:54 +0300

    Implement CurrencySource for EstonianBank

--- app/Services/Banks/EstonianBank.php
+++ app/Services/Banks/EstonianBank.php
@@ -2,12 +2,66 @@
 
 namespace App\Services\Banks;
 
+use Illuminate\Support\Facades\Http;
+
 class EstonianBank extends Bank
 {
     public static string $alias = "estonian_bank";
 
-    public function getTable(): string
+    public function getJsonCurrencyTable(): array
     {
-        return "Estonian Bank Table";
+        $xml_response = Http::get(env('BANK_ESTONIAN_CURRENCY_LINK'));
+
+        $xml_string = simplexml_load_string($xml_response);
+        $xml_like_json = json_decode(json_encode($xml_string), true);
+
+        $json = $this->prettifyJsonCurrencyTable($xml_like_json)['currencies'];
+
+        return ["data" => $json];
+    }
+
+    /**
+     * @param array $ugly_json
+     * @return array{currencies: array}
+     */
+    private function prettifyJsonCurrencyTable(array $ugly_json): array
+    {
+        $json = [];
+
+        foreach ($ugly_json as $key => $value)
+        {
+            $key = match ($key)
+            {
+                "Cube" => "currencies",
+                default => $key
+            };
+
+            if (gettype($value) === 'array')
+            {
+                $object = $this->prettifyJsonCurrencyTable($value);
+
+                if (count($object) === 1)
+                {
+                    $single_value = array_slice($object, 0, 1);
+                    if (gettype($single_value) === 'array')
+                    {
+                        $json = $single_value;
+                        continue;
+                    }
+                }
+
+                match ($key)
+                {
+                    "@attributes" => $json = array_merge([], $object),
+                    default => $json[$key] = $object
+                };
+            }
+            else
+            {
+                $json[$key] = $value;
+            }
+        }
+
+        return $json;
     }
 }
