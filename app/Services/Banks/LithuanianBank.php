e6ffba9ae531224a1740e0c53dc716fcc0cf3f62
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-08-30 19:03:33 +0300

    Implement CurrencySource for LithuanianBank

--- app/Services/Banks/LithuanianBank.php
+++ app/Services/Banks/LithuanianBank.php
@@ -2,12 +2,37 @@
 
 namespace App\Services\Banks;
 
+use Illuminate\Support\Facades\Http;
+
 class LithuanianBank extends Bank
 {
     public static string $alias = "lithuanian_bank";
 
-    public function getTable(): string
+    public function getJsonCurrencyTable(): array
     {
-        return "Lithuanian Bank Table";
+        $csv_response = Http::get(env('BANK_LITHUANIAN_CURRENCY_LINK'));
+
+        $csv_rows = array_map('str_getcsv', explode("\n", $csv_response));
+        $json = $this->getJsonCurrencyTableFromCsv($csv_rows);
+
+        return ["data" => $json];
+    }
+
+    private function getJsonCurrencyTableFromCsv(array $csv_rows): array
+    {
+        $json = ["imported" => $csv_rows[0][3]];
+        $currencies = [];
+
+        foreach ($csv_rows as $value)
+        {
+            array_push($currencies, [
+                "currency" => $value[1],
+                "rate" => $value[2]
+            ]);
+        }
+
+        $json['currencies'] = $currencies;
+
+        return $json;
     }
 }
