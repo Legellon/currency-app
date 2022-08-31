3fb3e241342f2faa0c05ec87aab29733bd0cbf00
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-08-31 18:27:10 +0300

    Add caching for LithuanianBank\getJsonCurrencyTable()

--- app/Services/Banks/LithuanianBank.php
+++ app/Services/Banks/LithuanianBank.php
@@ -3,26 +3,39 @@
 namespace App\Services\Banks;
 
 use Illuminate\Support\Facades\Http;
+use function App\Utils\Cache\getCachedOrCacheJsonFromRedis;
 
 final class LithuanianBank extends Bank
 {
     public static string $alias = "lithuanian_bank";
 
+    private static string $currency_table_key;
+
+    public function __construct()
+    {
+        self::$currency_table_key = self::$alias . ":currency_table";
+    }
+
     public function getJsonCurrencyTable(): array
     {
+        return getCachedOrCacheJsonFromRedis(
+            self::$currency_table_key,
+            10,
+            [self::class, 'downloadCurrencyTable']);
+    }
+
+    public static function downloadCurrencyTable(): array
+    {
         $csv_response = Http::get(env('BANK_LITHUANIAN_CURRENCY_URL'));
-
         $csv_rows = array_map('str_getcsv', explode("\n", $csv_response));
-        $json = $this->getJsonCurrencyTableFromCsv($csv_rows);
-
-        return ["data" => $json];
+        return self::getJsonCurrencyTableFromCsv($csv_rows);
     }
 
     /**
      * @param array $csv_rows
      * @return array{imported: string, currencies: array}
      */
-    private function getJsonCurrencyTableFromCsv(array $csv_rows): array
+    private static function getJsonCurrencyTableFromCsv(array $csv_rows): array
     {
         $json = ["imported" => $csv_rows[0][3]];
         $currencies = [];
