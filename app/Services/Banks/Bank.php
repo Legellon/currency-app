0aa9f25a0564fc307a040cfab6fffbd74f1f8538
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-09-01 15:28:41 +0300

    Implement CurrencyController for Bank

--- app/Services/Banks/Bank.php
+++ app/Services/Banks/Bank.php
@@ -2,7 +2,37 @@
 
 namespace App\Services\Banks;
 
-abstract class Bank implements CurrencySource
+abstract class Bank implements CurrencyCalculator
 {
     use HasAlias;
+
+    public function convert(string $from, string $to, float $amount, string $date): array
+    {
+        [$currency_table, $err] = $this->getJsonCurrencyTable($date);
+
+        if ($err)
+        {
+            return [0, true];
+        }
+
+        $from_rate = self::findCurrencyRate($from, $currency_table);
+        $to_rate   = self::findCurrencyRate($to, $currency_table);
+
+        if ($from_rate === false || $to_rate === false)
+        {
+            return [0, true];
+        }
+
+        $exchange_rate = $to_rate / $from_rate;
+
+        return [$exchange_rate * $amount, false];
+    }
+
+    private static function findCurrencyRate(string $currency, array $currency_table): float|false {
+        $index = array_search(
+            strtoupper($currency),
+            array_column($currency_table['currencies'], 'currency'));
+
+        return $index ? floatval($currency_table['currencies'][$index]['rate']) : false;
+    }
 }
